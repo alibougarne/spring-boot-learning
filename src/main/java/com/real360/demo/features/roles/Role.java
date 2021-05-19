b618aee0e41ea08a043a73459cb333bdd0b87b52
alibougarne <bougarne.ali@gmail.com>
2021-05-19 04:52:25 +0100

    add users and roles entities

---
+++ src/main/java/com/real360/demo/features/roles/Role.java
@@ -0,0 +1,39 @@
+package com.real360.demo.features.roles;
+
+import com.real360.demo.features.users.User;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Data
+@Table(name = "\"roles\"")
+public class Role {
+    @Id
+    @GeneratedValue
+    @Column(name = "id", nullable = false)
+    private UUID Id;
+
+    @Basic
+    @Column(name = "name", nullable = true, length = 40)
+    private String name;
+
+    @Basic
+    @Column(name = "created_at", nullable = true)
+    @Temporal(TemporalType.DATE)
+    private Date createddAt;
+
+    @Basic
+    @Column(name = "updated_at", nullable = true)
+    @Temporal(TemporalType.DATE)
+    private Date updatedAt;
+
+    // relations
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users = new HashSet<>();
+}
---
+++ src/main/java/com/real360/demo/features/users/User.java
@@ -0,0 +1,68 @@
+package com.real360.demo.features.users;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.real360.demo.features.roles.Role;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Data
+@Table(name = "\"users\"")
+public class User {
+    @Id
+    @GeneratedValue
+    @Column(name = "id", nullable = false)
+    private UUID Id;
+
+    @Basic
+    @Column(name = "first_name", nullable = true, length = 40)
+    private String firstName;
+
+    @Basic
+    @Column(name = "last_name", nullable = true, length = 40)
+    private String lastName;
+
+    @Basic
+    @Column(name = "email", nullable = true, length = 40)
+    private String email;
+
+    @Basic
+    @Column(name = "active")
+    private boolean isActive;
+
+    @Basic
+    @Column(name = "profile_picture", nullable = true)
+    private String profilePicture;
+
+    @Basic
+    @JsonIgnore
+    @Column(name = "password", nullable = true)
+    private String password;
+
+    @Basic
+    @Column(name = "created_at", nullable = true)
+    @Temporal(TemporalType.DATE)
+    private Date createddAt;
+
+    @Basic
+    @Column(name = "updated_at", nullable = true)
+    @Temporal(TemporalType.DATE)
+    private Date updatedAt;
+
+    // relations
+    @ManyToMany(cascade = {CascadeType.ALL})
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = {
+                    @JoinColumn(name = "role_id"),
+            },
+            inverseJoinColumns = {@JoinColumn(name = "user_id")}
+    )
+    Set<Role> roles = new HashSet<>();
+
+}
